11504f1aaab04cca241241e9ca02fbe0d1e368f5
229935 <229935@edu.p.lodz.pl>
2021-05-24 19:13:06 +0200

    first commit 8)

---
+++ functions.py
@@ -0,0 +1,13 @@
+from numpy import sin, cos, e
+
+f1 = lambda x: abs(2 * x - 3)
+
+f2 = lambda x: sin(x + 2)
+
+f3 = lambda x: cos(2*x)
+
+f4 = lambda x: x * x
+
+f5 = lambda x: cos(sin(x))
+
+w = lambda x: e ** (- (x * x))
